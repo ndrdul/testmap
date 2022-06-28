d91fbf0f5899ad59d10fddd6134ca80c43495708
ndrdul <devDrdul@protonmail.com>
2022-06-28 16:19:54 -0400

    working

--- src/MapChart.js
+++ src/MapChart.js
@@ -6,34 +6,59 @@ import {
   Geography,
 } from "react-simple-maps";
 
-import { scaleQuantize } from "d3-scale";
+import { scaleQuantize, scaleLinear } from "d3-scale";
 import { csv } from "d3-fetch";
 import { interpolatePiYG } from "d3-scale-chromatic";
 
+import { interpolateHcl } from "d3-interpolate";
+
 const colorScale = scaleQuantize()
-  .domain([1, 10])
+  .domain([1, 20])
   .range([
-    "#ffedea",
-    "#ffcec5",
-    "#ffad9f",
-    "#ff8a75",
-    "#ff5533",
-    "#e2492d",
-    "#be3d26",
-    "#9a311f",
-    "#782618",
+    "#ffffd9",
+    "#edf8b1",
+    "#c7e9b4",
+    "#7fcdbb",
+    "#41b6c4",
+    "#1d91c0",
+    "#225ea8",
+    "#253494",
+    "#081d58",
+    "#081d58",
   ]);
+
+// [
+//   "#ffedea",
+//   "#ffcec5",
+//   "#ffad9f",
+//   "#ff8a75",
+//   "#ff5533",
+//   "#e2492d",
+//   "#be3d26",
+//   "#9a311f",
+//   "#782618",
+// ]
+
+const colorScale2 = scaleLinear()
+  .domain([0, 90])
+  .range(["#EBFAFF", "cornflowerblue"])
+  .interpolate(interpolateHcl);
+
+const colorScale3 = scaleLinear()
+  .domain([0, 10])
+  .range(interpolatePiYG)
+  .interpolate(interpolateHcl);
 
 const MapChart = ({ setTooltipContent }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    csv("/inflationData.csv").then((counties) => {
-      setData(counties);
+    csv("/inflationData.csv").then((inflation) => {
+      setData(inflation);
     });
   }, []);
 
-  console.log(data);
+  //console.log(data);
 
   return (
     <>
@@ -42,26 +67,28 @@ const MapChart = ({ setTooltipContent }) => {
           <Geographies geography="/features.json">
             {({ geographies }) =>
               geographies.map((geo) => {
-                const cur = data.find(s => s.Country === geo.properties.name);
-                console.log(cur)
+                const cur = data.find((s) => s.Country === geo.properties.name);
+                
                 return (
                   <Geography
                     key={geo.rsmKey}
                     geography={geo}
-                    fill={colorScale(cur ? cur.Last : "#000000")}
+                    fill={colorScale(cur ? cur.Last : "#fcba03")}
                     onMouseEnter={() => {
-                      setTooltipContent(`${geo.properties.name} - ${cur.Country} - ${cur.Last} - Last Updated: ${cur.Reference}`);
+                      setTooltipContent(
+                        `${geo.properties.name} - ${cur.Country} - ${cur.Last} -  Last Updated: ${cur.Reference}`
+                      );
                     }}
                     onMouseLeave={() => {
                       setTooltipContent("");
                     }}
                     style={{
                       hover: {
-                        fill: "#F53",
-                        outline: "none",
+                        fill: "#27FB6B", 
+                        outline: "none", 
                       },
                       pressed: {
-                        fill: "#E42",
+                        fill: "#C2F970",
                         outline: "none",
                       },
                     }}
@@ -78,5 +105,5 @@ const MapChart = ({ setTooltipContent }) => {
 
 export default memo(MapChart);
 
-
 //https://codesandbox.io/s/o73pb?file=/src/MapChart.js:1087-1137
+//https://academy.datawrapper.de/article/134-what-to-consider-when-creating-choropleth-maps
