cb5e7382933a90dd8f477199cf41c38a1aa6a8f0
ndrdul <devDrdul@protonmail.com>
2022-06-28 13:02:09 -0400

    test map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { scaleSequential } from "d3-scale";
+import { interpolatePiYG } from "d3-scale-chromatic";
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ReactTooltip from "react-tooltip";
+
+import "./styles.css";
+
+import MapChart from "./MapChart";
+import {
+  ZoomableGroup,
+  ComposableMap,
+  Geographies,
+  Geography,
+} from "react-simple-maps";
+
+// function generateGdpPerCapita(geographies) {
+//   let minGdpPerCapita = Infinity;
+//   let maxGdpPercapita = -Infinity;
+//   geographies = geographies.map((geography) => {
+//     const { GDP_MD_EST, POP_EST } = geography.properties;
+//     const gdpPerCapita = Math.round((GDP_MD_EST * 1e6) / POP_EST);
+//     if (gdpPerCapita < minGdpPerCapita) {
+//       minGdpPerCapita = gdpPerCapita;
+//     }
+//     if (gdpPerCapita > maxGdpPercapita) {
+//       maxGdpPercapita = gdpPerCapita;
+//     }
+//     geography.properties.gdpPerCapita = gdpPerCapita;
+//     return geography;
+//   });
+//   return { minGdpPerCapita, maxGdpPercapita, modifiedGeographies: geographies };
+// }
+// const geoUrl =
+//   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 function App() {
+  const [content, setContent] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
---
+++ src/MapChart.js
@@ -0,0 +1,50 @@
+import React, { memo } from "react";
+import {
+  ZoomableGroup,
+  ComposableMap,
+  Geographies,
+  Geography
+} from "react-simple-maps";
+
+const MapChart = ({ setTooltipContent }) => {
+  return (
+    <>
+      <ComposableMap data-tip="">
+        <ZoomableGroup>
+          <Geographies geography="/features.json">
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    setTooltipContent(`${geo.properties.name}`);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  style={{
+                    default: {
+                      fill: "#D6D6DA",
+                      outline: "none"
+                    },
+                    hover: {
+                      fill: "#F53",
+                      outline: "none"
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      outline: "none"
+                    }
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </>
+  );
+};
+
+export default memo(MapChart);
